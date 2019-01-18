506d976ff4c2a2d88bba317e663788b262007676
unknown <qiuna.pan@woyihome.com>
2019-01-18 16:18:13 +0800

    two

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -5,11 +5,16 @@
       <div class="grade">
                    <tooltip :content="graded" :always="always" placement="top-start"
                             :offset="offset[1]">
-                     <img src="./grad.png" alt="">
+                     <img src="./grad.png" alt="" class="gradeimg">
                    </tooltip>
       </div>
-     <!-- <rate disabled v-model="valueDisabled" :count='count' :offset='offset'></rate> -->
-
+      <div class="grade">
+                   <tooltip :content="twos" :always="always" placement="top-start"
+                            :offset="offset[2]">
+                     <img src="./grad.png" alt="" class="gradeimg">
+                   </tooltip>
+      </div>
+     
     </div>
 </template>
 <script>
@@ -17,29 +22,32 @@ export default {
   data() {
     return {
       show: true,
-      // valueDisabled: 3,
-      //总数
-      count: 5,
-      offset: ["90", "90", "90"], 
+      offset: [600, 600, 600],
       always: true,
-      list: [{ number: 3.3 }],
+      list: [{ number: 4.0 }, { number: 2.0 }, { number: 4.5 }],
+      twos: "",
       graded: ""
     };
   },
   computed: {},
   methods: {},
   created() {
-    console.log(this.list);
     var listArr = this.list;
     for (var k in listArr) {
       var listSrt = listArr[k].number;
-      console.log(listSrt);
-      
-      // this.offset[1] = this.graded;
-      this.offset[1] =120 * listSrt;;
+      // console.log(listSrt)
+      console.log(listArr[1]);
+      var one = listArr[1].number;
 
-      this.graded= listSrt
-      console.log(this.graded);
+      var two = listArr[2].number;
+      console.log(two);
+      this.offset[1] = 145 * one;
+      this.graded=one
+
+      // this.offset[1] = 145 * listSrt;
+      // this.graded = listSrt;
+      this.offset[2] = 145 * two;
+      this.twos = two;
     }
   }
 };
@@ -53,13 +61,22 @@ p {
   padding: 5px;
 }
 .grade {
-  margin: 50px;
+  margin: 150px 100px;
+}
+.gradeimg {
+  width: 720px;
 }
 .ivu-tooltip-inner {
   width: 40px;
   background: coral;
+  margin-left: -25px;
 }
 .ivu-tooltip-popper[x-placement^="bottom"] .ivu-tooltip-arrow {
   border-bottom-color: coral;
+  margin-left: -25px;
+}
+.ivu-tooltip-popper[x-placement^="top"] .ivu-tooltip-arrow {
+  border-top-color: coral;
+  margin-left: -25px;
 }
 </style>
